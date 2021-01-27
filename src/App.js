89a693056afc7843020a0cbbb373425ec671d757
damarowen <damarowen@gmail.com>
2021-01-27 22:23:15 +0700

    add and delete done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,70 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import Add from './components/Add'
+
 
 function App() {
 
+    //* nameState,function
+    const [tasks, setTask] = useState(
+        [
+            {
+                id: 1,
+                text: 'Doctors Appointment',
+                day: 'Feb 5th at 2:30pm',
+                reminder: true,
+            },
+            {
+                id: 2,
+                text: 'Meeting at School',
+                day: 'Feb 6th at 2:30pm',
+                reminder: true,
+            },
+            {
+                id: 3,
+                text: 'Food Shopping',
+                day: 'Feb 7th at 2:36pm',
+                reminder: false,
+            }
+        ]
+    )
 
-  return (
-    <div className="container">
-    <Header/>
-    <Tasks/>
-    </div>
 
-  );
+    //*add task
+
+    const addTask = (x) => {
+     //* set random number
+     const id = Math.floor(Math.random() * 10000 )  + 1  
+    const baru = { id, ...x}
+    console.log(baru)
+    setTask([...tasks, baru])
+    }
+
+
+    //*delete task
+
+    const deleteTask = (id) => {
+        console.log(id)
+        //* menampilan yang id nya tidak sama
+        setTask(tasks.filter(x => x.id !== id))
+    }
+
+    //* Toggle Reminder
+    const toggleReminder = (id) => {
+        setTask(tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task))
+    }
+
+    return (
+        <div className="container">
+            <Header />
+            <Add tambah={addTask} />
+            {/* task and onDelete is a props */}
+            { tasks.length > 0 ?
+                (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : (' No Task to show')}
+        </div>
+
+    );
 }
 
 //* if use class
@@ -23,6 +77,8 @@ function App() {
 //     return <h1>heelo from a class</h1>
 //   }
 // }
+
+
 
 
 export default App;
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,5 +1,4 @@
-
-import { useState } from 'react'
+import Task from './Task'
 
 //* not use useState
 // const tasks = [
@@ -24,39 +23,14 @@ import { useState } from 'react'
 
 // ]
 
-const Tasks = () => {
-
-    //* nameState,function
-    const [tasks] = useState(
-        [
-            {
-                id: 1,
-                text: 'Doctors Appointment',
-                day: 'Feb 5th at 2:30pm',
-                reminder: true,
-            },
-            {
-                id: 2,
-                text: 'Meeting at School',
-                day: 'Feb 6th at 2:30pm',
-                reminder: true,
-            },
-            {
-                id: 3,
-                text: 'Food Shopping',
-                day: 'Feb 7th at 2:36pm',
-                reminder: false,
-            }
-        ]
-    )
-
-
+const Tasks = ({ tasks, onDelete, onToggle }) => {
 
     return (
         <>
             {tasks.map((x) => (
                 //* must have key prop
-                <h3 key={x.id}>{x.text}</h3>
+                <Task key={x.id} task={x} 
+                onDelete={onDelete} onToggle={onToggle}/>
             )
 
             )}
